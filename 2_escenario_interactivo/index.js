3eba42a748b810d3da27898f9b3e4e271a1eb5bd
DanielHincapie-cpu <hincapied286@gmail.com>
2025-10-21 17:24:14 -0500

    Avance escenario

---
+++ 2_escenario_interactivo/index.js
@@ -0,0 +1,25 @@
+const jell = document.querySelectorAll('.jellyfish');
+const score = document.querySelector('.score');
+const Reiniciar = document.querySelector('.reset');
+
+let counter = 0
+
+
+console.log(jell, score)
+
+jell.forEach( item => {
+
+
+    item.addEventListener('click', () => {
+    item.style.filter = "grayscale(1)"
+    item.classList.add('salto')
+    counter++
+    score.textContent = `Score: ${counter}`
+})
+
+ });
+
+reset.addEventListener('click', () => { 
+    counter = 0
+    score.textContent = `Reiniciar: ${counter}` 
+     });
