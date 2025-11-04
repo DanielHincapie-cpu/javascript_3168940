f6e54c0c0ee44eca2b0194eac142f77a2d1b8eba
DanielHincapie-cpu <hincapied286@gmail.com>
2025-11-04 17:18:47 -0500

    escenarios con sonidos

--- 2_escenario_interactivo/index.js
+++ 2_escenario_interactivo/index.js
@@ -2,55 +2,64 @@
 const jell = document.querySelectorAll('.jellyfish');
 const score = document.querySelector('.score');
 const Reiniciar = document.querySelector('.reset');
+const sonido = document.querySelector('.sonido');
 
 let counter = 0
 
 
 console.log(jell, score)
 
-jell.forEach( item => {
+jell.forEach(item => {
 
 
     item.addEventListener('click', () => {
-    item.style.filter = "grayscale(1)"
-    item.classList.add('salto')
-    counter++
-    score.textContent = `Score: ${counter}`
-})
+        sonido.play();
+        item.style.filter = "grayscale(1)"
+        item.classList.add('salto')
+        counter++
+        score.textContent = `Score: ${counter}`
 
- });
+    })
 
-reset.addEventListener('click', () => { 
+});
+
+reset.addEventListener('click', () => {
     counter = 0
-    score.textContent = `Puntos: ${counter}` 
-     });
-    
+    score.textContent = `Puntos: ${counter}`
+});
+
+
+
+
+
 //  escenario 2
 const content = document.querySelectorAll('.content');
 const Puntaje = document.querySelector('.Puntaje');
-const Resetear = document.querySelector('.Resetear'); 
+const Resetear = document.querySelector('.Resetear');
+const sonido2 = document.querySelector('.sonido2');
 
-let contador = 0 
+let contador = 0
 
 
 console.log(content, Puntaje)
 
-content.forEach( item => {
+content.forEach(item => {
 
 
     item.addEventListener('click', () => {
-    item.style.filter = "grayscale(1)"
-    item.classList.add('saltar')
-    contador++
-    Puntaje.textContent = `Puntaje: ${contador}`
-})
+        sonido2.play();
+        item.style.filter = "grayscale(1)"
+        item.classList.add('saltar')
+        contador++
+        Puntaje.textContent = `Puntaje: ${contador}`
+    })
 
- });
- 
+});
+
 Resetear.addEventListener('click', () => {
     contador = 0
-    Puntaje.textContent = `Reiniciar: ${contador}` 
-     });
+    Puntaje.textContent = `Reiniciar: ${contador}`
+});
 
 
 
@@ -58,26 +67,28 @@ Resetear.addEventListener('click', () => {
 const lang = document.querySelectorAll('.lang');
 const Points = document.querySelector('#Points');
 const Reempezar = document.querySelector('.Reempezar');
+const sonido3 = document.querySelector('.sonido3');
 
 let count = 0
 
 console.log(lang, Points)
 
-lang.forEach( item => {
+lang.forEach(item => {
 
     item.addEventListener('click', () => {
-    item.style.filter = "grayscale(1)"
-    item.classList.add('saltar')
-    count++
-    Points.textContent = `Puntos: ${count}`
-})
+        sonido3.play();
+        item.style.filter = "grayscale(1)"
+        item.classList.add('saltar')
+        count++
+        Points.textContent = `Puntos: ${count}`
+    })
 
- });
+});
 
-Reempezar.addEventListener('click', () => { 
+Reempezar.addEventListener('click', () => {
     count = 0
-    Points.textContent = `Puntos: ${count}` 
-     });
+    Points.textContent = `Puntos: ${count}`
+});
 // carrusel de escenas
 
 //varibales
@@ -85,15 +96,15 @@ const escenas = document.querySelectorAll('.escena');
 const btnSiguiente = document.querySelector('.btn-siguiente');
 const btnAnterior = document.querySelector('.btn-anterior');
 const miniaturas = document.querySelectorAll('.miniatura');
-let indice = 0 
+let indice = 0
 
 console.log(escenas, btnSiguiente, btnAnterior, miniaturas);
 
 //funciones
-function mostrarEscena (i) {
+function mostrarEscena(i) {
     for (let j = 0; j < escenas.length; j++) {
         escenas[j].classList.remove('activa');
-        
+
     }
     escenas[i].classList.add('activa');
 
@@ -101,16 +112,16 @@ function mostrarEscena (i) {
 
 };
 
-btnAnterior.addEventListener('click', function() {
-    indice = indice -1        
+btnAnterior.addEventListener('click', function () {
+    indice = indice - 1
     if (indice < 0) {
-        indice = escenas.length -1;
+        indice = escenas.length - 1;
     }
     mostrarEscena(indice);
 });
 
-btnSiguiente.addEventListener('click', function() { 
-    indice = indice +1        
+btnSiguiente.addEventListener('click', function () {
+    indice = indice + 1
     if (indice >= escenas.length) {
         indice = 0;
     }
@@ -119,8 +130,8 @@ btnSiguiente.addEventListener('click', function() {
 
 
 
-miniaturas.forEach(function(miniatura, i) {
-    miniatura.addEventListener('click', function() {
+miniaturas.forEach(function (miniatura, i) {
+    miniatura.addEventListener('click', function () {
         mostrarEscena(i);
     });
 });
