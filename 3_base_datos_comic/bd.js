bfece2c7062b1d11f1087dcae468552aae7f015c
DanielHincapie-cpu <hincapied286@gmail.com>
2025-11-05 17:21:31 -0500

    base de datos creada

---
+++ 3_base_datos_comic/bd.js
@@ -0,0 +1,42 @@
+let comic = {
+  "nombrecomic": "Trama Vortex",
+  "numerocapitulos": 3,
+  "genero": "Inspiración",
+  "sinopsis": "Super tower",
+  "autores": ["Juan Lopez", "Daniel Hincapié", "Juan Polo"],
+  "portadacomic": "images/portada.jpg",
+  "year": 2025,
+  "capitulos": [
+    {
+        "id": 1,
+      "nombre": "El Nacimiento de un Eco Musical",
+      "personajes": 29,
+      "portada": "Dan Jukes",
+      "descripcion": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+    {
+        "id": 2,
+      "nombre": "El Alma detrás de la Voz Inmortal",
+      "personajes": 39,
+      "portada": "Jane Wilson",
+      "descripcion": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+        "id": 3,
+      "nombre": "Un Legado que Resuena en el Presente",
+      "personajes": 1000000,
+      "portada": "Unknown",
+      "descripcion": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+}
